4e4ab2c44c9e0af7bcca0f675a80ba126fae383d
Sita Ram Sahu <103084791+sitaramsahu@users.noreply.github.com>
2025-10-28 15:37:20 +0530

    Update favicon and icon path in layout
    Removed old favicon from app directory and added new favicon at the project root. Updated layout.js to reference the new icon path.

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "                Read Yatra - Har Kitaab, Ek Nayi Yatra",
   description: "Read Yatra offers a wide range of books at unbeatable prices.",
+  icons: {
+    icon: "/readyatra-logo.png",
+  },
 };
 
 export default function RootLayout({ children }) {
